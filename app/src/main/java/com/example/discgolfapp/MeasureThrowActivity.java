5d89258d098f5ab44d8899a8963605294c3f724b
cscholzy <cscholz2016@gmail.com>
2020-03-23 20:05:26 -0500

    Create files for each feature (activity, controller, model)

---
+++ app/src/main/java/com/example/discgolfapp/MeasureThrowActivity.java
@@ -0,0 +1,14 @@
+package com.example.discgolfapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeasureThrowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measure_throw);
+    }
+}
---
+++ app/src/main/java/com/example/discgolfapp/MyBagActivity.java
@@ -0,0 +1,14 @@
+package com.example.discgolfapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyBagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_bag);
+    }
+}
