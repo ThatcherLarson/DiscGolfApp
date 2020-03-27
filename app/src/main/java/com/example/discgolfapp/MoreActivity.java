a74139b02276a758c79c9a8c7e6f2e07ad9482eb
Thatcher Larson <larsonthatch@gmail.com>
2020-03-26 18:04:43 -0600

    created initial intents to 'more' page activities

--- app/src/main/java/com/example/discgolfapp/MoreActivity.java
+++ app/src/main/java/com/example/discgolfapp/MoreActivity.java
@@ -2,7 +2,10 @@ package com.example.discgolfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MoreActivity extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class MoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more);
+
+        setTitle("More");
+
+        Button rulesBtn = (Button) findViewById(R.id.rulesBtn);
+        Button tutorialsBtn = (Button) findViewById(R.id.tutorialsBtn);
+        Button measureThrowBtn = (Button) findViewById(R.id.measureThrowBtn);
+
+        rulesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent rulesIntent = new Intent(getApplicationContext(), RulesActivity.class);
+                startActivity(rulesIntent);
+            }
+        });
+
+        tutorialsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent tutorialsIntent = new Intent(getApplicationContext(), TutorialsActivity.class);
+                startActivity(tutorialsIntent);
+            }
+        });
+
+        measureThrowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent measureThrowIntent = new Intent(getApplicationContext(), MeasureThrowActivity.class);
+                startActivity(measureThrowIntent);
+            }
+        });
+
     }
 }
