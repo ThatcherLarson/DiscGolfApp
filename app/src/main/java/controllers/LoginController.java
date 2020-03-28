2be5c3a2cc364f7ade2e5ddea1bfe1bee0a3eb88
cscholzy <cscholz2016@gmail.com>
2020-03-28 16:39:45 -0500

    refactored some code, added input validation for sign up, and added sign out functionality to ProfileActivity

--- app/src/main/java/controllers/LoginController.java
+++ app/src/main/java/controllers/LoginController.java
@@ -1,14 +1,20 @@
 package controllers;
 
 import android.content.Context;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginController {
     private FirebaseAuth auth;
     private Context context;
 
-    public LoginController(Context context) {
-        this.context = context;
-        auth = FirebaseAuth.getInstance();
+    public LoginController(FirebaseAuth auth) {
+        this.auth = auth;
+    }
+
+    public Task<AuthResult> loginUser(String email, String pass) {
+        return auth.signInWithEmailAndPassword(email, pass);
     }
 }
--- app/src/main/java/controllers/ProfileController.java
+++ app/src/main/java/controllers/ProfileController.java
@@ -1,4 +1,15 @@
 package controllers;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileController {
+    private FirebaseAuth auth;
+
+    public ProfileController(FirebaseAuth auth) {
+        this.auth = auth;
+    }
+
+    public void signOutUser() {
+        auth.signOut();
+    }
 }
