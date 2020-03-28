e94a1c6279f028f0b58ed0c6adc34fe68cc8cacc
Will Miller <williamjoemiller@gmail.com>
2020-03-28 14:30:27 -0500

    Adding array elements to course list java file

--- app/src/main/java/com/example/discgolfapp/CoursesActivity.java
+++ app/src/main/java/com/example/discgolfapp/CoursesActivity.java
@@ -17,10 +17,17 @@ public class CoursesActivity extends AppCompatActivity {
 
     private Button btnNewGame;
 
+    String s1[], s2[];
+    int images[] = {R.drawable.disc_golf_basic};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+
+        s1 = getResources().getStringArray(R.array.course_names);
+        s2 = getResources().getStringArray(R.array.course_descriptions);
 
         controller = new LoginController(this);
         model = new LoginModel();
