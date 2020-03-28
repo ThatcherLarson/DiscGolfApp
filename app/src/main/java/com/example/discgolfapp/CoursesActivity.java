562e61f24096ba2c238930ea955f006705f52689
Will Miller <williamjoemiller@gmail.com>
2020-03-28 13:58:24 -0500

    feature(iter1): Course List Setup

--- app/src/main/java/com/example/discgolfapp/CoursesActivity.java
+++ app/src/main/java/com/example/discgolfapp/CoursesActivity.java
@@ -3,12 +3,30 @@ package com.example.discgolfapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import controllers.LoginController;
+import models.LoginModel;
 
 public class CoursesActivity extends AppCompatActivity {
+    private LoginController controller;
+    private LoginModel model;
+    private FirebaseAuth auth;
+
+    private Button btnNewGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+
+        controller = new LoginController(this);
+        model = new LoginModel();
+        auth = FirebaseAuth.getInstance();
+
+        // initialize view elements
+        btnNewGame = findViewById(R.id.btnNewGame);
     }
 }
