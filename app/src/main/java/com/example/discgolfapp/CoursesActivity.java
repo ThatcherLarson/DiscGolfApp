a7fe6b5cdcf744022f2da390809b44a6cb6605d0
Will Miller <williamjoemiller@gmail.com>
2020-03-30 19:20:51 -0500

    Added OnClick() functionality to New Game button on Course page

--- app/src/main/java/com/example/discgolfapp/CoursesActivity.java
+++ app/src/main/java/com/example/discgolfapp/CoursesActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,7 +32,7 @@ public class CoursesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
 
-        controller = new CoursesController();
+        controller = new CoursesController(this);
         model = new CoursesModel();
         auth = FirebaseAuth.getInstance();
 
@@ -46,5 +48,11 @@ public class CoursesActivity extends AppCompatActivity {
         course_list.setLayoutManager(new LinearLayoutManager(this));
 
 
+        btnNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CoursesActivity.this, GamesActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/controllers/CoursesController.java
+++ app/src/main/java/controllers/CoursesController.java
@@ -1,4 +1,23 @@
 package controllers;
 
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.discgolfapp.CoursesActivity;
+import com.example.discgolfapp.GamesActivity;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class CoursesController {
+    private Context context;
+    private FirebaseAuth auth;
+
+
+    public CoursesController(Context context) {
+        this.context = context;
+        this.auth = auth;
+    }
+
+    public Intent clickNewGame() {
+        return new Intent(context, GamesActivity.class);
+    }
 }
