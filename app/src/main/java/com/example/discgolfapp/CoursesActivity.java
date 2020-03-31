477b463687523cfa641c80777c0be6a35d502852
Will Miller <williamjoemiller@gmail.com>
2020-03-30 22:27:34 -0500

    Added Options Menu for Navigation

--- app/src/main/java/com/example/discgolfapp/CoursesActivity.java
+++ app/src/main/java/com/example/discgolfapp/CoursesActivity.java
@@ -6,6 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -47,12 +50,36 @@ public class CoursesActivity extends AppCompatActivity {
         course_list.setAdapter(myAdapter);
         course_list.setLayoutManager(new LinearLayoutManager(this));
 
-
         btnNewGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(CoursesActivity.this, GamesActivity.class));
             }
         });
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.nav_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //respond to menu item selection
+        switch (item.getItemId()) {
+            case R.id.games:
+                startActivity(new Intent(this, GamesActivity.class));
+                return true;
+            case R.id.courses:
+                startActivity(new Intent(this, CoursesActivity.class));
+                return true;
+            case R.id.profile:
+                startActivity(new Intent(this, ProfileActivity.class));
+                return true;
+            case R.id.more:
+                startActivity(new Intent(this, MoreActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
